b5375d480d3d88bb012f81abeece06d822778254
Will <wgnann@gmail.com>
2019-06-18 18:53:54 -0300

    Agora o login correto cria usuário e chama Auth()

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Socialite;
 
@@ -46,6 +48,36 @@ class LoginController extends Controller
     public function handleProviderCallback()
     {
         $userSenhaUnica = Socialite::driver('senhaunica')->user();
-        var_dump($userSenhaUnica); die();
+
+        $user = User::where('nusp',$userSenhaUnica->codpes)->first();
+        if (is_null($user)) {
+            $user = new User;
+        }
+
+        # autorização por grupo
+        $authorized = false;
+        foreach ($userSenhaUnica->vinculo as $vinculo) {
+            if ($vinculo['siglaUnidade'] == "IME") {
+                if ($vinculo['tipoVinculo'] == 'SERVIDOR' or
+                    $vinculo['tipoVinculo'] == 'ALUNOPOS' or
+                    $vinculo['tipoVinculo'] == 'ALUNOPD') {
+                    $authorized = true;
+                }
+            }
+        }
+
+        # aqui ficará autorização individual, se necessário
+
+        if (!$authorized) {
+            return redirect('/');
+        }
+
+        $user->nusp = $userSenhaUnica->codpes;
+        $user->email = $userSenhaUnica->codpes.'@usp.br';
+        $user->name = $userSenhaUnica->nompes;
+        $user->save();
+
+        Auth::login($user, true);
+        return redirect('/');
     }
 }
