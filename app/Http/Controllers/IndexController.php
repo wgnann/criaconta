ed8d71be471b8d53e858675ff4ae0768aa79756a
Will <wgnann@gmail.com>
2019-06-26 14:00:52 -0300

    IndexController com Account

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Account;
+use Auth;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $account = NULL;
+        if (Auth::check()) {
+            $account = Account::where([
+                ['user_id', Auth::user()->id],
+                ['type', 'pessoal']
+            ])->first();
+        }
+        return view('index', compact('account'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,27 +6,59 @@
 
     @auth
         <div class="panel">
-            <table class="table table-sm">
-            <tbody>
-                <tr>
-                    <th>NUSP</th>
-                    <td>{{ Auth::user()->nusp }}</td>
-                </tr>
-                <tr>
-                    <th>Nome</th>
-                    <td>{{ Auth::user()->name }}</td>
-                </tr>
-                <tr>
-                    <th>email</th>
-                    <td>{{ Auth::user()->email }}</td>
-                </tr>
-                <tr class="table-warning">
-                    <th>status</th>
-                    <td>(virá no context)</td>
-                </tr>
-            </tbody>
-            </table>
-            <button type="button" class="btn btn-primary">Criar</button>
+            @if ($account)
+                <h4>Informações da conta pessoal</h4>
+                <table class="table table-sm">
+                <tbody>
+                    <tr>
+                        <th>Responsável</th>
+                        <td>{{ $account->user->nusp }}</td>
+                    </tr>
+                    <tr>
+                        <th>username</th>
+                        <td>{{ $account->username }}</td>
+                    </tr>
+                    <tr>
+                        <th>Nome</th>
+                        <td>{{ $account->name }}</td>
+                    </tr>
+                    <tr>
+                        <th>Grupo</th>
+                        <td>{{ $account->group->name }}</td>
+                    </tr>
+                    @if ($account->status)
+                        <tr class="table-success">
+                            <th>status</th>
+                            <td>ativa</td>
+                        </tr>
+                    @else
+                        <tr class="table-warning">
+                            <th>status</th>
+                            <td>não ativa</td>
+                        </tr>
+                    @endif
+                </tbody>
+                </table>
+            @else
+                <h4>Conta não criada</h4>
+                <table class="table table-sm table-info">
+                <tbody>
+                    <tr>
+                        <th>NUSP</th>
+                        <td>{{ Auth::user()->nusp }}</td>
+                    </tr>
+                    <tr>
+                        <th>Nome</th>
+                        <td>{{ Auth::user()->name }}</td>
+                    </tr>
+                    <tr>
+                        <th>email</th>
+                        <td>{{ Auth::user()->email }}</td>
+                    </tr>
+                </tbody>
+                </table>
+                <button type="button" class="btn btn-primary">Criar</button>
+            @endif
         </div>
     @else
         <div class="panel">
