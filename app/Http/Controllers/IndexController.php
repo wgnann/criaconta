a421a5dc044046afcbe58b71873b7e7f20c44998
Will <wgnann@gmail.com>
2019-06-26 18:57:43 -0300

    Selecionar grupo ao criar conta

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Account;
+use App\Group;
 use Auth;
 use Illuminate\Http\Request;
 
@@ -11,12 +12,14 @@ class IndexController extends Controller
     public function index()
     {
         $account = NULL;
+        $groups = NULL;
         if (Auth::check()) {
             $account = Account::where([
                 ['user_id', Auth::user()->id],
                 ['type', 'pessoal']
             ])->first();
+            $groups = Group::where('vinculo', Auth::user()->vinculo)->get();
         }
-        return view('index', compact('account'));
+        return view('index', compact('account', 'groups'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,68 +3,81 @@
 @section ('title') Cria Conta - IME-USP @endsection
 
 @section ('content')
-
-    @auth
-        <div class="panel">
-            @if ($account)
-                <h4>Informações da conta pessoal</h4>
-                <table class="table table-sm">
-                <tbody>
-                    <tr>
-                        <th>Responsável</th>
-                        <td>{{ $account->user->nusp }}</td>
-                    </tr>
-                    <tr>
-                        <th>username</th>
-                        <td>{{ $account->username }}</td>
-                    </tr>
-                    <tr>
-                        <th>Nome</th>
-                        <td>{{ $account->name }}</td>
-                    </tr>
-                    <tr>
-                        <th>Grupo</th>
-                        <td>{{ $account->group->name }}</td>
-                    </tr>
-                    @if ($account->status)
-                        <tr class="table-success">
-                            <th>status</th>
-                            <td>ativa</td>
-                        </tr>
-                    @else
-                        <tr class="table-warning">
-                            <th>status</th>
-                            <td>não ativa</td>
-                        </tr>
-                    @endif
-                </tbody>
-                </table>
-            @else
-                <h4>Conta não criada</h4>
-                <table class="table table-sm table-info">
-                <tbody>
-                    <tr>
-                        <th>NUSP</th>
-                        <td>{{ Auth::user()->nusp }}</td>
-                    </tr>
-                    <tr>
-                        <th>Nome</th>
-                        <td>{{ Auth::user()->name }}</td>
-                    </tr>
-                    <tr>
-                        <th>email</th>
-                        <td>{{ Auth::user()->email }}</td>
-                    </tr>
-                </tbody>
-                </table>
-                <button type="button" class="btn btn-primary">Criar</button>
-            @endif
-        </div>
-    @else
-        <div class="panel">
-            Faça o <a href="/login/senhaunica">login</a>!
-        </div>
-    @endauth
+  @auth
+    <div class="panel">
+      @if ($account)
+        <h4>Informações da conta pessoal</h4>
+        <table class="table table-sm">
+        <tbody>
+          <tr>
+            <th>Responsável</th>
+            <td>{{ $account->user->nusp }}</td>
+          </tr>
+          <tr>
+            <th>username</th>
+            <td>{{ $account->username }}</td>
+          </tr>
+          <tr>
+            <th>Nome</th>
+            <td>{{ $account->name }}</td>
+          </tr>
+          <tr>
+            <th>Grupo</th>
+            <td>{{ $account->group->name }}</td>
+          </tr>
+          @if ($account->status)
+            <tr class="table-success">
+              <th>status</th>
+              <td>ativa</td>
+            </tr>
+          @else
+            <tr class="table-warning">
+              <th>status</th>
+              <td>não ativa</td>
+            </tr>
+          @endif
+        </tbody>
+        </table>
+      @else
+        <h4>Conta não criada</h4>
+        <form action="{{ url('accounts') }}" method="post">
+          @csrf
+          <table class="table table-sm table-info">
+          <tbody>
+            <tr>
+              <th>NUSP</th>
+              <td>{{ Auth::user()->nusp }}</td>
+            </tr>
+            <tr>
+              <th>Nome</th>
+              <td>{{ Auth::user()->name }}</td>
+            </tr>
+            <tr>
+              <th>email</th>
+              <td>{{ Auth::user()->email }}</td>
+            </tr>
+            <tr>
+              <th>Grupo</th>
+              <td>
+                @foreach ($groups as $group)
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="group" value="{{ $group->id }}">
+                    <label class="form-check-label">{{ $group->name }}</label>
+                  </div>
+                @endforeach
+              </td>
+            </tr>
+          </tbody>
+          </table>
+          <button type="submit" class="btn btn-primary">Criar</button>
+        </form>
+      @endif
+    </div>
+  @else
+    <div class="panel">
+      Faça o <a href="/login/senhaunica">login</a>!
+    </div>
+  @endauth
 @endsection
 
 @section ('footer')
