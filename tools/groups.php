c96aa5e4df1ba0d2b550a0b1578e1c46064aceb7
Will <wgnann@gmail.com>
2019-06-27 11:34:33 -0300

    Exemplo para adicionar grupos

---
+++ tools/groups.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Group;
+
+$groups = [
+    ['Funcionário', 'func', 'SERVIDOR'],
+    ['Docente Computação', 'mac', 'SERVIDOR'],
+    ['Docente Estatística', 'mae', 'SERVIDOR'],
+    ['Docente Matemática Aplicada', 'map', 'SERVIDOR'],
+    ['Docente Matemática', 'mat', 'SERVIDOR'],
+    ['Pós Computação', 'posmac', 'ALUNOPOS'],
+    ['Pós Estatística', 'posmae', 'ALUNOPOS'],
+    ['Pós Matemática Aplicada', 'posmap', 'ALUNOPOS'],
+    ['Pós Matemática', 'posmat', 'ALUNOPOS'],
+    ['Pós Bioinformática', 'posbio', 'ALUNOPOS'],
+    ['Mestrado Profissional Ensino de Matemática', 'posmpem', 'ALUNOPOS'],
+    ['Pós Doutorado Computação', 'posdocmac', 'ALUNOPD'],
+    ['Pós Doutorado Estatística', 'posdocmae', 'ALUNOPD'],
+    ['Pós Doutorado Matemática Aplicada', 'posdocmap', 'ALUNOPD'],
+    ['Pós Doutorado Matemática', 'posdocmat', 'ALUNOPD'],
+];
+
+foreach ($groups as $group) {
+    $g = new Group();
+    $g->name = $group[0];
+    $g->code = $group[1];
+    $g->vinculo = $group[2];
+    if ($g->save()) {
+        echo "Adicionado: $g->code\n";
+    }
+}
