cfbf4a913d23a25140681aeaa0403d494b1676d0
Will <wgnann@gmail.com>
2019-07-24 18:28:23 -0300

    Contas institucionais separadas em criadas/não.

--- app/Http/Controllers/InstitutionalAccountController.php
+++ app/Http/Controllers/InstitutionalAccountController.php
@@ -13,7 +13,25 @@ class InstitutionalAccountController extends Controller
     public function index()
     {
         $nusp = Auth::user()->nusp;
-        $emails = IDMail::find_lists($nusp);
-        return view('institutional.index', compact('emails'));
+        $accounts = IDMail::find_lists($nusp);
+        $active = [];
+        $queued = [];
+        $todo = [];
+        foreach ($accounts as $account) {
+            $username = explode("@", $account['email'])[0];
+            $local_account = Account::where('username', $username)->first();
+            if (is_null($local_account)) {
+                $todo[] = $account;
+            }
+            else {
+                if ($local_account->ativo) {
+                    $active[] = $local_account;
+                }
+                else {
+                    $queued[] = $local_account;
+                }
+            }
+        }
+        return view('institutional.index', compact('todo', 'queued', 'active'));
     }
 }
--- resources/views/institutional/index.blade.php
+++ resources/views/institutional/index.blade.php
@@ -2,26 +2,88 @@
 
 @section ('title') Cria Conta - IME-USP @endsection
 
+@section ('styles')
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('/vendor/laravel-usp-theme/css/style.css')}}">
+  <style>
+    .table{table-layout:fixed;}
+  </style>
+@endsection
+
 @section ('content')
   @auth
     <h4>Informações das contas institucionais</h4>
     <div class="panel">
-      <table class="table table-sm">
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>Nome</th>
-          </td>
-        </thead>
-        <tbody>
-        @foreach ($emails as $email)
-          <tr>
-            <td>{{ $email['email'] }}</td>
-            <td>{{ $email['name'] }}</td>
-          </tr>
-        @endforeach
-        </tbody>
-      </table>
+      @if ($todo)
+        <div class="border border-info my-1 p-1">
+          <h5 class="text-info">Contas não criadas</h5>
+          <table class="table table-sm">
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>Nome</th>
+              </td>
+            </thead>
+            <tbody>
+            @foreach ($todo as $todo_account)
+              <tr>
+                <td>{{ $todo_account['email'] }}</td>
+                <td>{{ $todo_account['name'] }}</td>
+                <td>action</td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+      @endif
+      @if ($queued)
+        <div class="border border-danger my-1 p-1">
+          <h5 class="text-danger">Contas não ativadas</h5>
+          <table class="table table-sm">
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>Nome</th>
+                <th></th>
+              </td>
+            </thead>
+            <tbody>
+            @foreach ($queued as $queued_account)
+              <tr>
+                <td>{{ $queued_account->username }}@ime.usp.br</td>
+                <td>{{ $queued_account->name }}</td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+      @endif
+      @if ($active)
+        <div class="border border-success my-1 p-1">
+          <h5 class="text-success">Contas ativas</h5>
+          <table class="table table-sm">
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>Nome</th>
+                <th></th>
+              </td>
+            </thead>
+            <tbody>
+            @foreach ($active as $active_account)
+              <tr>
+                <td>{{ $active_account->username }}@ime.usp.br</td>
+                <td>{{ $active_account->name }}</td>
+                <td></td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+      @endif
+      @if (!$todo and !$queued and !$active)
+        <h5>Sem conta institucional.</h5>
+      @endif
     </div>
   @endauth
 @endsection
