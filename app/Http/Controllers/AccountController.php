71b5dd85e6e72e1a4ad86518b7bd93001e718482
Will <wgnann@gmail.com>
2019-07-29 19:19:14 -0300

    Verificação de auth() removida.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -22,15 +22,11 @@ class AccountController extends Controller
 
     public function index()
     {
-        $account = NULL;
-        $groups = NULL;
-        if (Auth::check()) {
-            $account = Account::where([
-                ['user_id', Auth::user()->id],
-                ['type', 'pessoal']
-            ])->first();
-            $groups = Group::where('vinculo', Auth::user()->vinculo)->get();
-        }
+        $account = Account::where([
+            ['user_id', Auth::user()->id],
+            ['type', 'pessoal']
+        ])->first();
+        $groups = Group::where('vinculo', Auth::user()->vinculo)->get();
         return view('account.index', compact('account', 'groups'));
     }
 
