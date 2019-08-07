36d84cce15066e0cbc19f1f22fe33cd5005bf54d
Will <wgnann@gmail.com>
2019-08-07 19:17:12 -0300

    Faltava adicionar método no non-auth.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -12,7 +12,7 @@ class AccountController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except('todoAccounts', 'activateAccount', 'cancelAccountRequest');
+        $this->middleware('auth')->except('todoAccounts', 'activateAccount', 'cancelAccountRequest', 'accountFromUsername');
     }
 
     private function authAPI(Request $request)
