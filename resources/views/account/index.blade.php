d3680927567e803c7613837d8c2d011950764bc4
Will <wgnann@gmail.com>
2025-02-04 11:17:25 -0300

    desenho novo para account.index blade

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,54 +1,14 @@
-@extends ('laravel-usp-theme::master')
+@extends('layouts.app')
 
 @section ('content')
-  @include('messages.errors')
-  <div class="panel">
-    <h4>Conta pessoal</h4>
-    <form action="{{ route('account.store') }}" method="post">
-      @csrf
-      <div class="border border-info my-1 p-1">
-        <h5 class="text-info">Criar conta pessoal</h5>
-        <table class="table table-sm table-borderless">
-          <tr>
-            <th>Número USP</th>
-            <td>{{ Auth::user()->codpes }}</td>
-          </tr>
-          <tr>
-            <th>Nome</th>
-            <td>{{ Auth::user()->name }}</td>
-          </tr>
-          @if ($idmail)
-            <tr>
-              <th>Login</th>
-              <td>{{ $username }}</td>
-            </tr>
-          @else
-            <tr>
-              <th>Login</th>
-              <td>
-                <input class="form-control" type="text" id="username" name="username" placeholder="nome de usuário" pattern="[A-Za-z0-9]*" title="Apenas letras ou números"/>
-              </td>
-            </tr>
-          @endif
-          <tr>
-            <th>Grupo</th>
-            <td>
-              @foreach ($groups as $group)
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" id="group-{{ $group->id }}" name="group" value="{{ $group->id }}">
-                  <label class="form-check-label" for="group-{{ $group->id }}">{{ $group->name }}</label>
-                </div>
-              @endforeach
-            </td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>
-              <button type="submit" class="btn btn-info">Criar</button>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </form>
-  </div>
+    <div class="panel">
+        <div class="card card-outline card-primary">
+            <div class="card-header text-white bg-info">
+                <h5 class="card-title">Criar conta pessoal</h5>
+            </div>
+            <div class="card-body">
+                @include('account.partials.personal')
+            </div>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/account/partials/personal.blade.php
@@ -0,0 +1,42 @@
+<div class="card">
+    <div class="card-header">
+        <h5 class="card-title">
+            Número USP: {{ Auth::user()->codpes }}
+        </h5>
+    </div>
+    <div class="card-body">
+        <form action="{{ route('account.store') }}" method="post">
+            @csrf
+            <div class="form-row">
+                <div class="form-group col">
+                    <label for="name">Nome</label>
+                    <input class="form-control" type="text" id="name" value="{{ Auth::user()->name }}" readonly />
+                </div>
+                <div class="form-group col">
+                    <label for="username">Login</label>
+                    @if ($idmail)
+                        <input class="form-control" type="text" id="username" value="{{ $username }}" readonly />
+                    @else
+                        <input class="form-control" type="text" id="username" placeholder="nome de usuário" pattern="[A-Za-z0-9]*" title="Apenas letras ou números." />
+                    @endif
+                </div>
+                <div class="form-group col">
+                    <label for="group">Grupo</label>
+                    <select class="custom-select" id="group" name="group">
+                        <option select>Selecionar grupo</option>>
+                        @foreach ($groups as $group)
+                            <option value="{{ $group->id }}">
+                            {{ $group->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col">
+                    <button type="submit" class="btn btn-info">Criar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
